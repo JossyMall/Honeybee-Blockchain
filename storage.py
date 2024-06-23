21a00bc16c37a8c7ba3b98eea2f36c97f51bd447
Joshua Tim <39429664+JossyMall@users.noreply.github.com>
2024-06-23 01:37:21 -0700

    basic decentralized storage functions using IPFS or similar technologies(work in progress)

---
+++ storage.py
@@ -0,0 +1,8 @@
+# storage.py
+
+class DecentralizedStorage:
+    def __init__(self):
+        pass
+
+    # Implement methods for storing and retrieving data from IPFS
+    # Example: store_transaction(), retrieve_transaction()
